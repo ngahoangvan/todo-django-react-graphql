e47cfa45effea0273a4a74978c68c6f474f6645c
vanngaCNTT <ngahv2222@gmail.com>
2019-09-17 15:56:06 +0700

    2019-09-17: GraphQL CRUD with Event App

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,7 +55,7 @@ DJANGO_APPS = (
 
 THIRD_PARTY_APPS = ("graphene_django",)
 
-LOCAL_APPS = ()
+LOCAL_APPS = ("source.events",)
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
