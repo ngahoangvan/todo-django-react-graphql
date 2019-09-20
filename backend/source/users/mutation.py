48d2a683973fcbaa20ba3f4510ed87510d032d00
vanngaCNTT <ngahv2222@gmail.com>
2019-09-20 10:16:37 +0700

    Update user mutation: validate email and password

--- backend/source/users/mutation.py
+++ backend/source/users/mutation.py
@@ -1,5 +1,7 @@
 import graphene
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import validate_email
 from graphql import GraphQLError
 from .models import UserImage, UserProfile
 from .types import UserType, UserProfileType, UserImageType
@@ -39,12 +41,17 @@ class CreateUser(graphene.Mutation):
     @staticmethod
     def mutate(root, info, userInput=None):
         ok = True
-        user_instace = update_create_instance(User(), userInput)
-        if userInput.profile:
-            update_create_instance(UserProfile.objects.create(user=user_instace), userInput.profile)
+        if userInput.username and userInput.password and userInput.email:
+            validate_email(userInput.email)
+            validate_password(userInput.password)
+            user_instace = update_create_instance(User(), userInput)
+            if userInput.profile:
+                update_create_instance(UserProfile.objects.create(user=user_instace), userInput.profile)
+            else:
+                UserProfile.objects.create(user=user_instace)
+            return CreateUser(ok=ok, user=user_instace)
         else:
-            UserProfile.objects.create(user=user_instace)
-        return CreateUser(ok=ok, user=user_instace)
+            raise GraphQLError("Invalid input")
 
 
 class UpdateUser(graphene.Mutation):
