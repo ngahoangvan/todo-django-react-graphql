6f4d5ffe8cd3e50dce3b4951e4c6d4df4f69af4d
Hoàng Văn Ngà <ngahv2222@gmail.com>
2020-02-03 16:57:35 +0700

    Config CI/CD for python, node

--- backend/source/article/models.py
+++ backend/source/article/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Publication(models.Model):
     title = models.CharField(max_length=30)
@@ -9,6 +10,7 @@ class Publication(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Article(models.Model):
     headline = models.CharField(max_length=100)
